0031208184484120376b2b0f3ff2478d05799bf4
sujohn77 <stonebo0sh56@gmail.com>
2023-10-23 18:00:40 +0300

    initial working commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// import {NavigationContainer} from '@react-navigation/native';
+
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import QuizScreen from './screens/quizz/Quiz';
+
+// import CryptoListingScreen from './screens/crypto';
+// const Stack = createNativeStackNavigator();
+// import {QueryClient, QueryClientProvider} from 'react-query';
+import {Text, View} from 'react-native';
+
+// const queryClient = new QueryClient();
+const App = () => {
+  return (
+    <View>
+      <Text>test</Text>
+    </View>
+  );
+};
+
+export default App;
